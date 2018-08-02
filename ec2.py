94f7327d33c4f5716dac30fde30d458a7bfea491
Ubuntu <ubuntu@ip-172-31-21-42.us-east-2.compute.internal>
2018-08-02 16:24:56 +0000

    first commit from the ec2 instance

--- ec2.py
+++ ec2.py
@@ -1,14 +1,16 @@
-# -*- coding: utf-8 -*-
 """
-Created on Fri May 25 12:05:36 2018
-
-operations:
-    
-1) select columns to keep
-2) filter for building class
-3)    
-    
-TODO: ensure that all dataframes are aligned properly    
+- Initial column, BC, financial filtering of best filings from sas file
+- calculate continuous variables:
+a) expense ratio; diagnostics and standardization
+b) owner occupied ratio
+- encode categorical variables:
+a) building class
+b) previous year determination (PENDING)
+c) identity of filer
+d) filer lease type determination
+e) occurrence of physical construction (PENDING)
+f) expense categories - expense types are grouped by name
+- import the response variable - actual determinations made by assessors for current FY
 """
 
 import pymongo
@@ -20,40 +22,54 @@ import numpy as np
 from sklearn import  preprocessing
 from sklearn import model_selection
 
-#"mongodb://ian:secretPassword@123.45.67.89/cool_db
 mongo= pymongo.MongoClient('mongodb://luis:persyy@18.221.52.113/rpie')
 db=mongo.rpie
 collect=db.inc_2015
 
 df=collect.find()
 df1=pd.DataFrame(list(df))
-df1.dtypes
+
+#importing from SAS... filer15 data is submitted on 6/16 and applies to FY 17/18... the tentative valuation associated is 01/17 and the final roll is 6/17; and so a CAMA pull around that date corresponds to "current determination" (response) and CAMA data from 6/16 is previous yr determination (what we're) using as an independent variable
+path='G:/Property _CommercialModeling/SAS Data Sets/'
+best_15=pd.read_sas(path+'ie_re_best_filer15.sas7bdat')
+
+#importing from EC2
+#defaults to port 27017
+client = pymongo.MongoClient("mongodb://luis:persyy@127.0.0.1:8080/rpie")    
+db = client.rpie
+#point to the appropriate collection
+filings=db['filings']
+#retrieve only 2015 filings
+df_15=filings.find({'filings':{'$elemMatch':{'IE_YEAR':'2015'}}})
 
 #filter the desired columns
-cut=df1.filter(regex='(FILER_RELATN|TOT_INC|IE_TC|IE_BC|_INC|_EXP|BORO|BLOCK|LOT|IE_SEQ|IE_YEAR|UPDTNO|TRPL_NET_LEASE)',axis=1)
+cut=best_15.filter(regex='(FILER_RELATN|TOT_INC|IE_TC|IE_BC|_INC|_EXP|BORO|BLOCK|LOT|IE_SEQ|IE_YEAR|UPDTNO|TRPL_NET_LEASE)',axis=1)
 #remove all "condo blocks" fields, ie CND_BLOCK
 cnd_block=re.compile('^(?!CND_BLOCK).')
-cut=cut.filter(regex=cnd_block,axis=1) 
+cut=cut.filter(regex=cnd_block,axis=1)
 
 '''remove incomplete filings and those outside of the study's purview..
 a) remove coops, tax-exempt, vacant land, TC 1AC, 2ABC
 b) no income information... all inc and exp categories are None
 c) no total expense and income   
-    '''
-cut['IE_BC']=cut['IE_BC'].str.decode("utf-8")
+'''
 
+#converting certain string fields... this may not be necessary when querying from the mongodb
+cut['IE_BC']=cut['IE_BC'].str.decode("utf-8")
 cut['BLOCK']=cut['BLOCK'].astype(int).astype(str)
 cut['LOT']=cut['LOT'].astype(int).astype(str)
 cut['BORO']=cut['BORO'].str.decode("utf-8")
+cut['bbl']=cut.apply(lambda x: int(x['BORO'])*1000000000 + int(x['BLOCK'])*10000 + int(x['LOT']),axis=1)
+
+#BC filtering
 #negative lookbehind... remove observations with BC starting w/below letters... could inform this step by extracting all NNN from Vision and keeping only those BCs
 #bc=re.compile('(?<!V|A|B|C|D|H|I|J|M|N|P|Q|T|U|W|Y|Z).')
 bc1=re.compile('^(V|A|B|C|D|H|I|J|M|N|P|Q|T|U|W|Y|Z)')
 test=cut['IE_BC'].str.contains(bc1)
-cut['IE_BC'].head()
 cut1=cut[~test]  
 
+#encrypting account numbers... no longer implementing this 
 encoded_bbls=cut1.apply(lambda x: str.encode('-'.join([x['BORO'],x['BLOCK'],x['LOT']])),axis=1)
-
 hashed_bbls=encoded_bbls.apply(lambda x: hashlib.sha256(x).hexdigest())
 cut1['key']=hashed_bbls
 cut1.drop(['BORO','BLOCK','LOT'],inplace=True,axis=1)
@@ -61,29 +77,22 @@ cut1.drop(['BORO','BLOCK','LOT'],inplace=True,axis=1)
 lista=cut1.iloc[8:,].to_dict(orient='records')
 #print db.cool_collection.count()
 
-''' connecting to mongodb from work '''
-client = pymongo.MongoClient("mongodb://luis:persyy@127.0.0.1:8080/rpie") # 
-db = client.rpie
-vals15=db['inc_2015']
-#records insertion
-for i in lista:
-    vals15.insert_one(i)
     
 ''' CALCULATE CONTINUOUS VARIABLES '''
 # 1) Calculating expense ratio... will be standardized via a robust scalar
 #impute a '1' should the filer report 0 income
-cut1.loc[df1['F_RE_TOT_INC']==0.0,'F_RE_TOT_INC']=1
+cut1.loc[cut1['F_RE_TOT_INC']==0.0,'F_RE_TOT_INC']=1
 #calculate expense ratio
 cut1['exp_ratio']=cut1['F_TOT_EXP']/cut1['F_RE_TOT_INC']
 
 #visualize expense ratio on this dataset
-plt.hist(df1['exp_ratio'],bins=25)
+plt.hist(cut1['exp_ratio'],bins=25)
+plt.title('Distribution of expense ratio - untreated')
 plt.show()
 #remove outliers approach
 np.percentile(cut1['exp_ratio'].values,[25,50,75,95,98])
 #13,110 will be established as the upper bound
 ninetyeight=np.percentile(cut1['exp_ratio'].values,[98])[0]
-
 trunc=cut1.loc[cut1['exp_ratio']<ninetyeight,:]
 
 # 2) OWNER occupied income... ratio: owner occupied income / total income
@@ -92,11 +101,55 @@ trunc['own_occ_ratio']=trunc['OWN_OCC_INC']/trunc['F_RE_TOT_INC']
 trunc.loc[pd.isnull(trunc['own_occ_ratio']),'own_occ_ratio']=0
 
 #quality check the ratio... it looks fine, with outliers in an acceptable range
-plt.hist(cut1['own_occ_ratio'],bins=25)
+plt.hist(trunc['own_occ_ratio'],bins=25)
 plt.show()
 
+# square footage
 ''' encode catgorical variables '''
-#convert datatype of the object fields to type 'category'... each category is assigned an integer value... I could use this interger value for the label encoding... "cat.codes" attribute of the new column will be of type integer
+''' convert datatype of the object fields to type 'category'... each category is assigned an integer value... I could use this interger value for the label encoding... "cat.codes" attribute of the new column will be of type integer
+a) building class
+b) previous year determination
+c) identity of filer
+d) their own determination
+e) occurrence of physical construction    
+
+    '''
+''' identify parcels undergoing physical improvement '''
+dob=pd.read_sas('G:/Property_ORGPROJ/Orgproj/AnalyticsGrp/tom/scr/adhoc_06192018/dob.sas7bdat')
+
+''' end physical improvements queries '''
+
+''' import categorical variable: previous year determination - which should impact and be recorded in the FY 17/18 db'''
+
+
+
+'''  end import categorical variable: previous year determination '''
+
+
+''' import previous year categorizations '''
+cats=pd.read_csv('G:/Property/Luis_C/TripleNetLease/RPIE_cama_datasets/nnn_classification_201617.csv',skiprows=1, header=None,names=['pid','Boro','Block','Lot','filing','lease','tc','bc','sqft'])
+cats.describe()
+cats.dtypes
+cats['bbl']=cats.apply(lambda x: int(x['Boro'])*1000000000 + int(x['Block'])*10000 + int(x['Lot']),axis=1)
+cats['bbl'].head()
+cats['lease'].value_counts()
+cats.sort_values('pid').head(20)
+#merge these via a left join... if a property does not havea lease type on record from previous year, mark an "na"
+cut1_l=cut1.merge(cats[['bbl','lease','sqft']],on='bbl',how='inner')
+''' end previous year categorizations '''
+
+
+
+
+
+
+''' RESPONSE VARIABLE: import lease type determinations for 2017/18 '''
+leaseType_18=pd.read_csv('G:/Property/Luis_C/TripleNetLease/RPIE_cama_datasets/tripleNetLease_allValued_2018_II.csv',skiprows=1)
+leaseType_18.columns=['value_src','boro','block','lot','tc','bc','lease_type']
+leaseType_18['bbl_key']=leaseType_18.apply(lambda x: x['boro']*1000000000+x['block']*10000+x['lot'],axis=1)
+leaseType_18.head()
+#merge these to the training dataset... this is the response variable
+''' end importing lease type determinations '''
 
 '''one-hot encoding or dummy variables '''
 #Building class dummies
@@ -169,6 +222,9 @@ vals=train['exp_ratio'].values
 vals_r=vals.reshape(len(vals),1)
 robust_scaled_series=scaler_exp_ratio.fit_transform(vals_r)
 
+plt.hist(robust_scaled_series,bins=25)
+plt.show()
+
 scaler_own_occ= preprocessing.RobustScaler()
 vals=train['own_occ_ratio'].values
 vals_r=vals.reshape(len(vals),1)
@@ -193,4 +249,4 @@ sert=pd.Series(re_scaledt)
     a) sklearn.linear_model.LogisticRegression
     b) statsmodels.discrete.discrete_model
 '''    
-
+ 
